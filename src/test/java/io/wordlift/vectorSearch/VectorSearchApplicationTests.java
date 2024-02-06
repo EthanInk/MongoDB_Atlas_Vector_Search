23e0c411600b678ee04d6264df48486dba4f69e4
Ethan Kleinlugtenbelt <Ethankleinlugtenbelt@gmail.com>
2024-02-06 11:23:18 +0100

    init

---
+++ src/test/java/io/wordlift/vectorSearch/VectorSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wordlift.vectorSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
